b5615b26716e31eb34d22f87284b858b347d09b0
Ohata-renesas <tomoyuki.ohata.uh@renesas.com>
2020-09-06 12:20:52 +0900

    Updates

---
+++ precache-manifest.67c6daef9825662df2de256d7879cebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cebc39d6c3ef622ba86057d3dca30e",
+    "url": "/rx23w_hmi_demo_app/index.html"
+  },
+  {
+    "revision": "66f4bf3247f874e4aed9",
+    "url": "/rx23w_hmi_demo_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cd0be4a6045c181142b",
+    "url": "/rx23w_hmi_demo_app/static/js/2.5bf4bf71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rx23w_hmi_demo_app/static/js/2.5bf4bf71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f4bf3247f874e4aed9",
+    "url": "/rx23w_hmi_demo_app/static/js/main.1d05e015.chunk.js"
+  },
+  {
+    "revision": "17b4835a88e3548bd877",
+    "url": "/rx23w_hmi_demo_app/static/js/runtime-main.2263d826.js"
+  }
+]);
